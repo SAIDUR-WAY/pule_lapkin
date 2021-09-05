f83851c220dac38e8c2c9a432bad6082e5b11e1e
saidur_way <saiduronway@gmail.com>
2021-09-05 11:19:41 +0600

    My first web

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+// hamburger menu
+function openMenu(){
+      document.getElementById('menu_container').style.width="100%";
+}
+function closeMenu(){
+      document.getElementById('menu_container').style.width="0%";
+}
+// Mixitup
+var mixer = mixitup('.work_grid');
+  
